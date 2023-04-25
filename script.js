e4d50ea5dbd26b1d0e3e5cd5aa338e095711471e
Bryan Chow <bchow.post@gmail.com>
2023-04-25 22:45:02 +0100

    Finish finish coding

--- script.js
+++ script.js
@@ -4,9 +4,13 @@ confirmPassword = document.getElementById('confirm_password')
 var check = function() {
     if (password.value == "" || confirmPassword.value =="") {
         document.getElementById("message").textContent = ""
+        password.style.border = "0.5px solid gray";
+        confirmPassword.style.border = "0.5px solid gray";
     } else if(password.value == confirmPassword.value) {
       document.getElementById('message').style.color = 'green';
       document.getElementById('message').textContent = 'Passwords match';
+      password.style.border = "0.5px solid gray";
+      confirmPassword.style.border = "0.5px solid gray";
     } else {
       document.getElementById('message').style.color = 'red';
       document.getElementById('message').textContent = '*Passwords do not match';
